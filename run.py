f6ad2e125eeb0ab8e4d1adb65f8ddaa5404f13b4
a-wgn <antonwgn1993@gmail.com>
2023-01-22 21:34:22 +0000

    Check import of words

--- run.py
+++ run.py
@@ -1,3 +1,7 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+"""
+Import libraries to this application to run the game 
+"""
+import random 
+from words import words 
+
+print(words)

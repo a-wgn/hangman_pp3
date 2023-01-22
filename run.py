9c16fc159e857cef894720c9494f317dae84cc6e
a-wgn <antonwgn1993@gmail.com>
2023-01-22 21:42:04 +0000

    Add hangman def an input for terminal

--- run.py
+++ run.py
@@ -1,7 +1,28 @@
 """
-Import libraries to this application to run the game 
+Import libraries to this application to run the game.
 """
-import random 
-from words import words 
+import random
+from words import words
 
-print(words)
+
+def get_a_word(words):
+    """
+    Randomly choose a word from the list.
+    Keeps iterating back and forth until it is not true anymore.
+    Returns a word that those not have a word with dash or space
+    """
+    word = random.choice(words)
+    while '-' in word or ' ' in word:
+        word = random.choice(words)
+
+    return word
+
+
+def hangman():
+    word = get_a_word(words)
+    word_letter = set(word)
+    alphabet = set(string.ascii_uppercase)
+    used_letter = set()
+
+user_input = input('Type something')
+print(user_input)
